33eed73ea2935f1c78c77728c22ff048cf7f6b64
Artūras <artur.marcisevski@voicehit.com>
2018-07-20 16:52:36 +0300

    Router, images, default page

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,18 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    ButtonsModule
+    ButtonsModule,
+    RouterModule.forRoot([{
+      path: '',
+      component: HomeComponent
+    },
+      {
+        path: 'home',
+        component: HomeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
