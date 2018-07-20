7429b8fc3b8d19b6f4aecd88dfd0d3bdaa0661a9
Artūras <arturas.marcisauskas@gmail.com>
2018-07-20 23:20:09 +0300

    Footer, background fixes

--- angular/wwwroot/polyfills.js
+++ angular/wwwroot/polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\angular\angular\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\Art1z.Artis-PC\Desktop\angular\angular\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
